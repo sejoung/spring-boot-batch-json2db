92d60dbd7a4f98ee76a45cb89c81bd22959fc45f
sejoung kim <sejoung@gmail.com>
2018-09-10 13:39:44 +0900

    수정

--- src/main/java/com/github/sejoung/configuration/BatchConfiguration.java
+++ src/main/java/com/github/sejoung/configuration/BatchConfiguration.java
@@ -1,31 +1,44 @@
 package com.github.sejoung.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+import com.github.sejoung.Incrementer.CurrentTimeIncrementer;
+import com.github.sejoung.listener.ChunkExecutionListener;
+import com.github.sejoung.listener.JobCompletionNotificationListener;
+import com.github.sejoung.listener.StepExecutionNotificationListener;
+
 /**
  * @author kim se joung
  *
  */
 //@Configuration
-public class BatchConfiguration {
-/*
+public class BatchConfiguration extends DefaultBatchConfigurer {
+    
+ /*   
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
 
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
 
-   // @Value("audiencelog.log")
-    private Resource[] resources;
-    
-  //  @Value("${threadpool.size}")
-    private int poolsize;
-
-    @Bean(name = "audienceADLogLineMapper")
-    public AudienceADLogLineMapper audienceADLogLineMapper() {
-        return new AudienceADLogLineMapper();
-    }
-    @Bean(name = "audienceADLogItemProcessor")
-    public AudienceADLogItemProcessor audienceADLogItemProcessor() {
-        return new AudienceADLogItemProcessor();
+     (non-Javadoc)
+     * @see org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer#setDataSource(javax.sql.DataSource)
+     * 
+     * 스프링 배치에서 배치 정보를 저장하는 DB를 사용하지 않을려고 데이터 소스를 set 하는 부분을 주석 처리함
+     * 
+     
+    @Override
+    public void setDataSource(DataSource dataSource) {
     }
 
     @Bean
@@ -42,24 +55,23 @@ public class BatchConfiguration {
     public JobCompletionNotificationListener jobExecutionListener() {
         return new JobCompletionNotificationListener();
     }
-    
+
     @Bean
     public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
-        return jobBuilderFactory.get("importUserJob").incrementer(new RunIdIncrementer()).listener(listener).flow(step1).end().build();
+        return jobBuilderFactory.get("importUserJob").incrementer(new CurrentTimeIncrementer()).listener(listener).flow(step1).end().build();
     }
-
+    
     @Bean
-    public Step step1(MultiResourceItemReader<AudienceADLog> audienceADLogReader, TaskExecutor taskExecutor, AudienceADLogItemProcessor audienceADLogItemProcessor, ChunkExecutionListener chunkListener, StepExecutionNotificationListener stepExecutionListener) {
-        return stepBuilderFactory.get("step1").<AudienceADLog, AudienceADLog>chunk(10).reader(audienceADLogReader).processor(audienceADLogItemProcessor).taskExecutor(taskExecutor).listener(chunkListener).listener(stepExecutionListener).throttleLimit(poolsize).build();
+    public Step step1(Tasklet tasklet1) {
+        return stepBuilderFactory.get("step1").tasklet(tasklet1).build();
     }
-
-    @Bean(name = "audienceADLogReader")
-    public MultiResourceItemReader<AudienceADLog> audienceADLogReader(AudienceADLogLineMapper audienceADLogLineMapper) {
-
-        FlatFileItemReader<AudienceADLog> fileReader = new FlatFileItemReader<AudienceADLog>();
-        fileReader.setLineMapper(audienceADLogLineMapper);
-
-        return new MultiResourceItemReaderBuilder<AudienceADLog>().name("audience").delegate(fileReader).resources(resources).build();
+    
+    @Bean
+    public Tasklet tasklet1() {
+        return (contribution, chunkContext)->{
+            return RepeatStatus.FINISHED;
+        };
     }
+    
     */
 }
--- src/main/java/com/github/sejoung/repository/StatsTargetContainRepository.java
+++ src/main/java/com/github/sejoung/repository/StatsTargetContainRepository.java
@@ -18,4 +18,6 @@ public interface StatsTargetContainRepository extends MongoRepository<StatsTarge
             "        }\r\n" + 
             "    }")
     public List<StatsTargetContain> findAll();
+
+
 }
